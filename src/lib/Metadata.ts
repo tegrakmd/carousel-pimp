e5ef330f954f64e8188d40def9a1848610fca033
tegrakmd <tegrakamanda@gmail.com>
2026-06-08 20:12:29 +0200

    new commit meta data + OpenGraph

---
+++ src/lib/Metadata.ts
@@ -0,0 +1,45 @@
+import { Metadata } from "next";
+
+export function constructMetaData({
+  title = "Carousel Pimp ",
+  description = "Triy first try on a carousel pimp.",
+  image = "/ogimage.png",
+  icons = "/favicon.ico",
+  noIndex = false,
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+  noIndex?: boolean;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "@tegrakmd",
+    },
+    icons,
+    metadataBase: new URL("https://pint-three.vercel.app/"),
+    themeColor: "#131313",
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  };
+}
